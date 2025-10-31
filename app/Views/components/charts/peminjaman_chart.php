b131d747f1596f7e64d9aece03db029d5c42ea10
Kazuhaa11 <fahmy8384@gmail.com>
2025-10-31 13:39:17 +0700

    minore

--- app/Views/components/charts/peminjaman_chart.php
+++ app/Views/components/charts/peminjaman_chart.php
@@ -1,6 +1,6 @@
 <?php  ?>
 <div class="card mb-4">
-  <div class="card-header"><i class="fas fa-chart-bar me-2"></i> Peminjaman Storage (6 Bulan)</div>
+  <div class="card-header"><i class="fas fa-chart-bar me-2"></i> Peminjaman Storage (12 Bulan)</div>
   <div class="card-body">
     <canvas id="chartPeminjaman" height="120"></canvas>
   </div>
