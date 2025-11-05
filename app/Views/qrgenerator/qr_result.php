1f218f48c8811f48f4ef7a4d83bb338f11b2cc61
Kazuhaa11 <fahmy8384@gmail.com>
2025-11-05 11:42:27 +0700

    Revert "ww"
    This reverts commit 9bad0cb8e66c4c038d0f6328aa68d0a37dd0e13b.

--- app/Views/qrgenerator/qr_result.php
+++ app/Views/qrgenerator/qr_result.php
@@ -30,7 +30,7 @@
 <style>
     .qr-grid {
         display: grid;
-        grid-template-columns: repeat(4, 1fr);
+        grid-template-columns: repeat(3, 1fr);
         gap: 8mm;
     }
 
@@ -43,26 +43,26 @@
     }
 
     .qr-title {
-        font-weight: 400;
-        font-size: 6pt;
+        font-weight: 700;
+        font-size: 12pt;
         margin-bottom: 3mm;
     }
 
     .qr-desc {
-        font-size: 6pt;
+        font-size: 9pt;
         color: #666;
         margin-top: 2mm;
     }
 
     .qr-card img {
-        width: 15mm;
-        height: 15mm;
+        width: 45mm;
+        height: 45mm;
         object-fit: contain;
     }
 
     @page {
         size: A4;
-        margin: 5mm;
+        margin: 10mm;
     }
 
     @media print {
@@ -79,11 +79,11 @@
             position: absolute;
             left: 0;
             top: 0;
-            width: calc(210mm - 20mm); 
+            width: calc(210mm - 20mm);
         }
 
         .qr-grid {
-            grid-template-columns: repeat(4, 1fr);
+            grid-template-columns: repeat(3, 1fr);
             gap: 8mm;
         }
 
